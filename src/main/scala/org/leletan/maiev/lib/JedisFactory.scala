312fda1a7a1c060e6697710a3753c386ab6a0aac
jiale.tan <jiale.tan@vungle.com>
2018-02-05 17:52:50 -0800

    doc(redshift2redis): add commented out async way example for redis interaction

--- src/main/scala/org/leletan/maiev/lib/JedisFactory.scala
+++ src/main/scala/org/leletan/maiev/lib/JedisFactory.scala
@@ -10,11 +10,11 @@ import redis.clients.jedis.{Jedis, JedisPool, JedisPoolConfig}
 object JedisFactory
   extends RedisClientConfig
     with SafeConfig
-    with Logger{
+    with Logger {
 
   lazy private val jedisPool = {
 
-    info (s"jedis pool config: redisHost: $redisHost, redisPort: $redisPort, redisAuth: $redisAuth, redisDB: $redisDB")
+    info(s"jedis pool config: redisHost: $redisHost, redisPort: $redisPort, redisAuth: $redisAuth, redisDB: $redisDB")
 
     new JedisPool(
       {
@@ -29,8 +29,51 @@ object JedisFactory
       redisDB
     )
   }
+
   def getConn: Jedis = {
     jedisPool.getResource
   }
 
 }
+
+// import org.spark_project.guava.util.concurrent.RateLimiter
+// import scala.concurrent.duration.Duration
+// import scala.concurrent.{Await, Future}
+//object test extends App {
+//
+//  lazy val redisAuth: String = {
+//    val auth = ""
+//    if (auth.isEmpty) {
+//      null
+//    } else
+//      auth
+//  }
+//
+//  val jedisPool = new JedisPool(
+//    {
+//      val pc = new JedisPoolConfig()
+//      pc.setMaxTotal(128)
+//      pc
+//    },
+//    "xxx.yyy.zzz.aa",
+//    6379,
+//    5000,
+//    null,
+//    0
+//  )
+//
+//  val rateLimiter = RateLimiter.create(3.0)
+//
+//  import scala.concurrent.ExecutionContext.Implicits.global
+//
+//  Await.result(
+//  Future.sequence(
+//  (1 to 10).map{
+//  i =>
+//    Future[Unit] {
+//      rateLimiter.acquire()
+//      println(s"thread $i: ${jedisPool.getResource.getDB}")
+//    }
+//  }), Duration.Inf)
+//
+//}
